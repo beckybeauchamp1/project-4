877e10fac7b1cf198169c8b96e93291847c700c5
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-01-11 23:21:02 -0500

    Add Form for New Event

--- campus_events_backbone/js/backbone/views/college.js
+++ campus_events_backbone/js/backbone/views/college.js
@@ -6,7 +6,6 @@ App.Views.College = Backbone.View.extend({
     this.listenTo(this.model, 'change', this.render);
     this.listenTo(this.model.events, 'add', this.renderEvent);
     this.template = Handlebars.compile($("#collegeTemplate").html());
-    this.eventTemplate = Handlebars.compile($("#eventFormTemplate").html());
   },
   render: function(){
     App.Routers.college.navigate('colleges/' +  this.model.id);
@@ -16,7 +15,13 @@ App.Views.College = Backbone.View.extend({
   renderCollege: function(){
     App.Routers.college.navigate('colleges/' + this.model.id);
     this.$el.html(this.template(this.model.toJSON()));
-    this.$el.append(this.eventTemplate(this.model.toJSON()));
+    this.toggleEventForm();
+  },
+  toggleEventForm: function(){
+    var self = this;
+    $(".toggle-event-form").on("click", function(){
+      $("#new-event-modal").show();
+    });
   },
   hideSearch: function(){
     this.$el.empty();
@@ -27,19 +32,5 @@ App.Views.College = Backbone.View.extend({
     console.log("Render Event in College.js vIEW");
     var eventView = new App.Views.Event({model: event});
     this.$el.find(".events-class-div").append(eventView.$el);
-  },
-  createEvent: function(){
-    event.preventDefault();
-    var eventData = {
-      title: this.$("[name='authorName']").val(),
-      image: this.$("[name='content']").val(),
-      content: this.$("[name='content']").val(),
-      start_date: this.$("[name='content']").val(),
-      end_date: this.$("[name='content']").val(),
-      starting_time: this.$("[name='content']").val(),
-      ending_time: this.$("[name='content']").val(),
-      cost: this.$("[name='content']").val()
-    };
-    this.model.events.create(eventData);
   }
 });
